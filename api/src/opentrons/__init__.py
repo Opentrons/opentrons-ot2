d7c1adf52f444c0793ed1f56eaebe2b633fbfaed
Seth Foster <seth@opentrons.com>
2026-02-09 10:52:27 -0500

    chore: add a log to start actions

--- api/src/opentrons/__init__.py
+++ api/src/opentrons/__init__.py
@@ -147,6 +147,7 @@ async def initialize() -> ThreadManagedHardware:
     robot_conf = robot_configs.load()
     logging_config.log_init(robot_conf.log_level)
 
+    log.info(f'OT-2 Fork')
     log.info(f"API server version: {version}")
     log.info(f"Robot Name: {name()}")
 
